7568f40bf57d5e7c037c755199096c5d1f736894
Wellington <wellington@wellimovel.com>
2024-10-01 00:35:30 -0300

    limpezatestes

--- src/main/kotlin/com/example/backend/dto/NamesDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.backend.dto
-
-data class NamesDto(
-    val id: Long,
-    val name: String
-)
-
-
-
---
+++ src/main/resources/db/migration/V1__create_table_modal.sql
@@ -0,0 +1,6 @@
+
+create table MODAL (
+    id bigint not null auto_increment,
+    namemodal varchar(50) not null,
+    primary key(id)
+);
